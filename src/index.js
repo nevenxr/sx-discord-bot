56a6cd01efc4ccd38ab175835bb9478358a6e877
nevenxr <nevenxr@gmail.com>
2022-04-21 21:15:31 +0600

    refactor(handlers): exec handlers functions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 const Discord = require("discord.js");
-const fs = require("fs");
 
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MESSAGES"]
 });
 
-(() => {
-    let files = fs.readdirSync(require.main?.path + "/events")
-        .filter(ext => ext.endsWith(".js"));
-
-    for (let i = 0; i < files.length; i++) {
-        let event = require(`./events/${files[i]}`);
-        
-        if (typeof event === "object" && event.name) {
-            client.on(event.name, (...args) => event.exec(client, ...args));
-        };
-    };
-})();
+// Event Handler
+require("./utils/handlers").setEvents(client);
+// Command Handler
+require("./utils/handlers").setCommands(client);
 
 client.login(require("./secrets.json").bot_token);
